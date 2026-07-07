a86bfe1d361c9fbfd6593ed60fd498637c8cb9b9
bentirher <bentihe@gmail.com>
2026-07-07 09:34:54 +0200

    modify to run only last n = 5 tasks

--- oqs_generalization/submit_small.sh
+++ oqs_generalization/submit_small.sh
@@ -5,8 +5,8 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=20G
-#SBATCH --time=04:00:00
-#SBATCH --array=0-11
+#SBATCH --time=08:00:00
+#SBATCH --array=8-11
 #SBATCH --output=logs/small_%A_%a.out
 #SBATCH --error=logs/small_%A_%a.err
 
